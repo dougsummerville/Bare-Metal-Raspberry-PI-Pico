3b2d342fdf30bf7d4bbf059518dfb21b09c879f5
Douglas Summerville <dsummer@binghamton.edu>
2023-08-08 15:11:06 -0400

    Update usbcdc.c
    Removed dead LED code that was used for testing in USBCDC driver

--- drivers/usbcdc.c
+++ drivers/usbcdc.c
@@ -317,33 +317,7 @@ typedef struct{
 #define usb_device ((usb_device_t *)(usbram.ep0_buffer_opt))
 
 
-void configure_led()
-{
-    resets -> clr_reset = RESETS_RESET_IO_BANK0_MASK;
-    while(! (resets -> reset_done & RESETS_RESET_IO_BANK0_MASK))
-    	continue;
-    // GPIO clock at reset is clk_sys
-    io_bank0 -> gpio25_ctrl = 
-	IO_BANK0_GPIO25_CTRL_IRQOVER(0) |
-	IO_BANK0_GPIO25_CTRL_INOVER(0)  |
-	IO_BANK0_GPIO25_CTRL_OEOVER(0)  |
-	IO_BANK0_GPIO25_CTRL_OUTOVER(0) |
-	IO_BANK0_GPIO25_CTRL_FUNCSEL(5);
-    sio -> gpio_oe_set = (1<<25);
-}
-void turn_on_led()
-{
-    sio -> gpio_out_set = (1<<25);
-}
-void turn_off_led()
-{
-    sio -> gpio_out_clr = (1<<25);
-}
-void toggle_led()
-{
-    sio -> gpio_out_xor = (1<<25);
-}
-/*Helper function to perform the frequent step of maing out buffer avail and
+/*Helper function to perform the frequent step of making out buffer avail and
  * toggling the enpoiints DATA01 parity
  */
 static void free_ep_output_buf_and_toggle_data_sync( uint8_t ep )
