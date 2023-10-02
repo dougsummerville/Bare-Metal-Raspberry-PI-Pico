5d9ed8adbbaf90c52ce1c50ab27a8dbde3baa4af
Douglas Summerville <dsummer@binghamton.edu>
2023-10-02 17:38:54 -0400

    Update test_usbcdc.c
    Added comment on driver limitation

--- src/test_usbcdc.c
+++ src/test_usbcdc.c
@@ -4,6 +4,10 @@ int main()
 {
 	char c;
 	configure_usbcdc();
+	/* Calling usbcdc_putchar() will fail immediately after configure,
+	 * likely because the device has not yet enumerated.  The driver does
+	 * not poll until enumeration so as not to block
+	 */
 	while(1)
 	{
 		if( usbcdc_getchar(&c))
